1d08ea8d8d4b3e50e2cf1f7e52c37ba32dde2fba
mf-dlcz <90648025+mf-dlcz@users.noreply.github.com>
2022-01-23 01:25:19 -0800

    updated HTML, CSS, and JS.

--- src/script.js
+++ src/script.js
@@ -1,23 +1,44 @@
-function displayDate() {
-	let day = now.getDate();
+function displayDate(timestamp) {
+	let date = new Date(timestamp);
 	let hour = now.getHours();
 	if (hour < 10) {
 		hour = `0${hour}`;
 	}
-	let min = now.getMinutes();
+	let min = date.getMinutes();
 	if (min < 10) {
 		min = `0${min}`;
 	}
-	let dayofWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-	let daysOfWeek = dayofWeek[now.getDay()];
+	let dayofWeek = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
 
-	return `${daysOfWeek} ${day}, ${hour}:${min} `;
+	let daysOfWeek = dayofWeek[date.getDay()];
+	return `${daysOfWeek} ${date}, ${hour}:${min}`;
 }
 
-let now = new Date();
-let date = document.querySelector("#date");
+function displayFahrenheitTemp(event) {
+	event.preventDefault();
+	let tempEl = document.querySelector("#numb");
 
-date.innerHTML = displayDate(date);
+	celsiusLink.classList.remove("active");
+	fahrenheitLink.classList.add("active");
+	let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
+	tempEl.innerHTML = Math.round(fahrenheitTemp);
+}
+
+function displayCelsiusTemp(event) {
+	event.preventDefault();
+	celsiusLink.classList.add("active");
+	fahrenheitLink.classList.remove("active");
+	let tempEl = document.querySelector("#numb");
+	tempEl.innerHTML = Math.round(celsiusTemp);
+}
 
 function searchCity(city) {
 	let apiKey = "8478c376956a3e191ae5d6af99d9891c";
@@ -27,18 +48,15 @@ function searchCity(city) {
 
 function cityInput(event) {
 	event.preventDefault();
-	let city = document.querySelector("#city-input").value;
-	searchCity(city);
+	let city = document.querySelector("#city-input");
+	searchCity(city.value);
 }
-
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", cityInput);
 
 function displayForecast() {
 	let forecastElement = document.querySelector("#forecast");
 
-	let forecastHTML = `<div class="row">`;
 	let days = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday"];
+	let forecastHTML = `<div class="row">`;
 	days.forEach(function (day) {
 		forecastHTML =
 			forecastHTML +
@@ -47,8 +65,8 @@ function displayForecast() {
 						<div class="weather-forecast-date">${day}</div>
 							⛅
 							<div class="weather-forecast-temp">
-								<span class="max-temp">68◦ | </span>
-								<span class="min-temp">54◦</span>
+								<span class="max-temp"> 68° </span>
+								<span class="min-temp"> 54° </span>
 							</div>
 					</div>
 	`;
@@ -59,41 +77,39 @@ function displayForecast() {
 }
 
 function showTemp(response) {
-	console.log(response.data.name);
-	document.querySelector("#city").innerHTML = response.data.name;
-	document.querySelector("#numb").innerHTML = Math.round(
-		response.data.main.temp
+	let numbEl = document.querySelector("#numb");
+	let cityEl = document.querySelector("#city");
+	let conditionEl = document.querySelector("#condition");
+	let humidityEl = document.querySelector("#humidity");
+	let windEl = document.querySelector("#wind");
+	let dateEl = document.querySelector("#date");
+	let iconEl = document.querySelector("#icon");
+
+	celsiusTemp = response.data.main.temp;
+
+	numbEl.innerHTML = Math.round(celsiusTemp);
+	cityEl.innerHTML = response.data.name;
+	conditionEl.innerHTML = response.data.weather[0].description;
+	humidityEl.innerHTML = response.data.main.humidity;
+	windEl.innerHTML = Math.round(response.data.wind.speed);
+	dateEl.innerHTML = displayDate(response.data.dt * 1000);
+	iconEl.setAttribute(
+		"src",
+		`http:\\openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
 	);
-	document.querySelector("#humidity").innerHTML =
-		response.data.main.humidity;
-	document.querySelector("#wind").innerHTML = Math.round(
-		response.data.wind.speed
-	);
-	document.querySelector("#condition").innerHTML =
-		response.data.weather[0].main;
-	document
-		.querySelector("#icon")
-		.setAttribute(
-			"src",
-			`http:\\openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-		);
+	iconEl.setAttribute("alt", response.data.weather[0].description);
 }
 
-let currentLocationBtn = document.querySelector("#currentBtn");
-currentLocationBtn.addEventListener("click", getCurrentLocation);
+let celsiusTemp = null;
 
-function searchLocation(position) {
-	let latitude = position.coords.latitude;
-	let longitude = position.coords.longitude;
-	let units = "imperial";
-	let apiKey = "8478c376956a3e191ae5d6af99d9891c";
-	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
-	axios.get(apiUrl).then(showTemp);
-}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", cityInput);
 
-function getCurrentLocation(event) {
-	event.preventDefault();
-	navigator.geolocation.getCurrentPosition(searchLocation);
-}
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemp);
+
 searchCity("Tokyo");
 displayForecast();
