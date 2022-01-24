d3b90a6907e88750bc3358baa68126ba7854d0ff
mf-dlcz <90648025+mf-dlcz@users.noreply.github.com>
2022-01-23 18:10:17 -0800

    Displayed the forecast data using the API

--- src/script.js
+++ src/script.js
@@ -52,24 +52,36 @@ function cityInput(event) {
 	searchCity(city.value);
 }
 
+function formatDay(timestamp) {
+	let date = new Date(timestamp * 1000);
+	let day = date.getDay();
+	let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+	return days[day];
+}
 function displayForecast(response) {
+	let sevenDayforecast = response.data.daily;
 	let forecastElement = document.querySelector("#forecast");
 
-	let days = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday"];
 	let forecastHTML = `<div class="row">`;
-	days.forEach(function (day) {
-		forecastHTML =
-			forecastHTML +
-			`
+	sevenDayforecast.forEach(function (forecastDay, index) {
+		if (index < 6) {
+			forecastHTML =
+				forecastHTML +
+				`
 					<div class="col-2">
-						<div class="weather-forecast-date">${day}</div>
-							⛅
+						<div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+							<img
+							src="http://openweathermap.org/img/wn/${
+								forecastDay.weather[0].icon
+							}@2x.png" alt="" width="42" />
 							<div class="weather-forecast-temp">
-								<span class="max-temp"> 68° </span>
-								<span class="min-temp"> 54° </span>
+								<span class="max-temp">${Math.round(forecastDay.temp.max)} </span>
+								<span class="min-temp">${Math.round(forecastDay.temp.min)}</span>
 							</div>
 					</div>
 	`;
+		}
 	});
 
 	forecastHTML = forecastHTML + `</div>`;
